a3563b192eb66b1f3c425c25e9c5505eb22372c2
Steven Lehrburger <lehrburger@gmail.com>
2012-07-17 01:32:31 -0400

    Add logout link, token persistence with SQLAlchemy

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ source bin/activate
 ./bin/easy_install gunicorn
 ./bin/easy_install flask
 ./bin/easy_install Flask-OAuth
+./bin/pip mysql-python
+./bin/pip sqlalchemy
 git clone https://github.com/lehrblogger/vine-web.git web
 cd web
 ../bin/gunicorn -w 4 myapp:app -b 0.0.0.0:8000
@@ -15,7 +17,7 @@ cd web
 For Vagrantfile
 ---------------
   config.vm.forward_port 8000, 8000
-  
+
 To Run
 ------
 cd /vagrant/web-env/
--- myapp.py
+++ myapp.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for, request, flash
 from flaskext.oauth import OAuth
+from sqlalchemy import create_engine, select, MetaData, Table
 app = Flask(__name__)
 
+dbhost = 'localhost'
+dbuser = 'flask1'
+dbpass = 'ish9gen8ob8hap7ac9hy'
+dbname = 'vine'
+engine = create_engine('mysql+mysqldb://' + dbuser + ':' + dbpass + '@' + dbhost + '/' + dbname)
+metadata = MetaData()
+metadata.bind = engine
+users = Table('users', metadata, autoload=True)
+conn = engine.connect()
 
 oauth = OAuth()
 twitter = oauth.remote_app('twitter',
@@ -14,11 +24,10 @@ twitter = oauth.remote_app('twitter',
 )
 
 @app.route("/")
-def home():
-    user = None
+def index():
+    user = session.get('twitter_user')
     tweets = None
-    if 'twitter_token' in session and session['twitter_token']:
-        user = session['twitter_user']
+    if user:
         resp = twitter.get('statuses/home_timeline.json')
         if resp.status == 200:
             tweets = resp.data
@@ -29,25 +38,39 @@ def home():
 
 @twitter.tokengetter
 def get_twitter_token():
-    return session.get('twitter_token')
+    s = select([users.c.twitter_token, users.c.twitter_secret], users.c.name == session.get('twitter_user'))
+    return conn.execute(s).fetchone()
 
 @app.route('/login')
 def login():
     return twitter.authorize()
     #return twitter.authorize(callback=url_for('oauth_authorized', next=request.args.get('next') or request.referrer or None))
 
+@app.route('/logout')
+def logout():
+    session.pop('twitter_user', None)
+    flash('You were signed out')
+    return redirect(request.referrer or url_for('index'))
+
 @app.route('/twitter/oauth_callback')
 @twitter.authorized_handler
 def oauth_authorized(resp):
-    next_url = url_for('home') # request.args.get('next') or url_for('index')
+    next_url = url_for('index') # request.args.get('next') or url_for('index')
     if resp is None:
         flash(u'You denied the request to sign in.')
         return redirect(next_url)
-
-    session['twitter_token'] = (
-        resp['oauth_token'],
-        resp['oauth_token_secret']
-    )
+    s = select([users.c.twitter_token, users.c.twitter_secret], users.c.name == resp['screen_name'])
+    found_user = conn.execute(s).fetchone()
+    if found_user:
+        if not found_user.twitter_token == resp['oauth_token'] or not found_user.twitter_secret == resp['oauth_token_secret']:
+            u = users.update().\
+                      where(users.c.name == resp['screen_name']).\
+                      values(twitter_token=resp['oauth_token'], twitter_secret=resp['oauth_token_secret'])
+            conn.execute(u)
+    else:
+        i = users.insert().\
+                  values(name=resp['screen_name'], twitter_token=resp['oauth_token'], twitter_secret=resp['oauth_token_secret'])
+        conn.execute(i)
     session['twitter_user'] = resp['screen_name']
 
     flash('You were signed in as %s' % resp['screen_name'])
