84bc53df112735f2411fab7e74bdd93a818aa4b1
Steven Lehrburger <lehrburger@gmail.com>
2012-08-31 01:12:06 -0400

    Add message & /command signals to edge calculation
    Uses the shiny new logging system and data model to compute directed edge weights between users.
    
    Uses mostly-arbitrary weighting functions to create a total relationship weight from messages sent as well as topic changes, whispers, invites, and kicks.
    
    Also, get rid of the Sender and Recipient objects that were just wrappers around defaultdicts - it's easier/cleaner to use the straight dictionaries, and it's also easier to print for debugging.
    
    Next I'm going to use the edge weights to update the current vinebots with /new and /del_friendship.
    
    Note that there's some hacky stuff going on to filter out failed commands. Since the commands db entry is already inserted by the time we actually try to execute the command, it has no way to know if it's failed, and the best it can do is pretend to have succeeded so that handle_msg doesn't create a *second* entry for that same command, one with is_valid=true and one with is_valid=false. Later I might update the stored command's is_successful column (which doesn't exist yet), but the extra database write call seems like it's not worth the complexity for now. In the meantime, just remember that messages in response to /commands that start with "Sorry, " might be excluded from edge weight calculations!

--- calc_edges.py
+++ calc_edges.py
@@ -12,36 +12,6 @@ import sleekxmpp
 THRESHOLD = 0
 PAGESIZE = 20
 
-class Recipients(object):
-    def __init__(self):
-        self._recipients = defaultdict(int)
-        
-    def increment(self, recipient):
-        self._recipients[recipient] += 1
-        
-    def has_edge(self, recipient, message_threshold):
-        return self._recipients[recipient] > message_threshold
-    
-    def delete_edge(self, recipient):
-        if recipient in self._recipients:
-            del self._recipients[recipient]
-
-
-class Senders(object):
-    def __init__(self):
-        self._senders = defaultdict(Recipients)
-
-    def increment(self, sender, recipient):
-        self._senders[sender].increment(recipient)
-        
-    def has_edge(self, sender, recipient, message_threshold=THRESHOLD):
-        return self._senders[sender].has_edge(recipient, message_threshold)
-    
-    def delete_edge(self, sender, recipient):
-        if sender in self._senders:
-               self._senders[sender].delete_edge(recipient)
-        
-
 class EdgeCalculator(sleekxmpp.ClientXMPP):
     def __init__(self):
         sleekxmpp.ClientXMPP.__init__(self, '%s@%s' % (constants.graph_xmpp_user, constants.server), constants.graph_xmpp_password)
@@ -56,35 +26,43 @@ class EdgeCalculator(sleekxmpp.ClientXMPP):
 
     def start(self, event):
         self.process_logs()
-        self.update_next_old_edge()
-        
-    # override disconnect to make sure we finished and log errors if not
-    # think through failure cases
+        #self.update_next_old_edge()
+        self.cleanup()  # to disconnect when finished
+    
+    #TODO think through failure cases
 
     def process_logs(self):
-        self.senders = Senders()
-        offset = 0
-        logs = True
-        while logs:
-            logging.info('next')
-            logs = self.db_fetch_logs(offset)
-            offset += PAGESIZE
-            for log in logs:
-                logging.info(log)
-                self.senders.increment(log[0], log[1])
+        self.senders = defaultdict(lambda : defaultdict(int))
+        def process_log_type(db_query_fn, multiplier_fn):
+            offset = 0
+            messages = True
+            while messages:
+                messages = db_query_fn(offset)
+                offset += PAGESIZE
+                for message in messages:
+                    sender, recipient, body = message
+                    logging.info((sender, recipient, body))
+                    self.senders[sender][recipient] += multiplier_fn(body)
+        process_log_type(self.db_fetch_messages, lambda s: len(s))
+        process_log_type(self.db_fetch_topics,  lambda s: len(s))
+        process_log_type(self.db_fetch_whispers, lambda s: 2 * len(s))
+        process_log_type(self.db_fetch_invites,  lambda s: 100)
+        process_log_type(self.db_fetch_kicks,  lambda s: -100)
+        
+        import json
+        logging.info('\n' + json.dumps(self.senders, indent=4))
     
     def update_next_old_edge(self):
-        pass
-        # old_edge = # fetch next edge with offset = self.old_edge_offset, limit = 1
-        # if old_edge:
-        #     if self.senders.has_edge(old_edge.frm, old_edge.to):
-        #         self.senders.delete_edge(old_edge.frm, old_edge.to)
-        #         self.update_next_old_edge()
-        #     else:
-        #         self.send_message_to_leaf('/del_friendship %s %s' % (user1, user2))
-        # else:
-        #     self.in_delete_phase = False
-        #     self.update_next_new_edge()
+        old_edge = # fetch next edge with offset = self.old_edge_offset, limit = 1
+        if old_edge:
+            if self.senders.has_edge(old_edge.frm, old_edge.to):
+                self.senders.delete_edge(old_edge.frm, old_edge.to)
+                self.update_next_old_edge()
+            else:
+                self.send_message_to_leaf('/del_friendship %s %s' % (user1, user2))
+        else:
+            self.in_delete_phase = False
+            self.update_next_new_edge()
     
     def update_next_new_edge(self):
         new_edge = []# pop one from  self.senders:
@@ -110,36 +88,105 @@ class EdgeCalculator(sleekxmpp.ClientXMPP):
         msg['body'] = body
         msg.send()
     
-    def db_fetch_logs(self, offset):
-        messages = self.db_execute_and_fetchall("""SELECT sender.name, recipient.name, logs.body
-                                                   FROM logs, log_recipients, users AS sender, users AS recipient
-                                                   WHERE logs.id = log_recipients.log_id
-                                                   AND sender.id = logs.author_id
-                                                   AND recipient.id = log_recipients.recipient_id
-                                                   AND logs.sent_on > %(startdate)s
-                                                   LIMIT %(pagesize)s
-                                                   OFFSET %(offset)s""",
-                                                   {'startdate': 0, 'pagesize': PAGESIZE, 'offset': offset})
-        whispers = self.db_execute_and_fetchall("""SELECT logs.id, sender.name, recipient.name, logs.body
-                                                   FROM logs, log_recipients, users AS sender, users AS recipient
-                                                   WHERE logs.id = log_recipients.log_id
-                                                   AND sender.id = logs.author_id
-                                                   AND recipient.id = log_recipients.recipient_id
-                                                   AND logs.sent_on > %(startdate)s
-                                                   LIMIT %(pagesize)s
-                                                   OFFSET %(offset)s """,
-                                                   {'startdate': 0, 'pagesize': PAGESIZE, 'offset': offset})
-        # topics = self.db_execute_and_fetchall("""  SELECT logs.id, sender.name, recipient.name, logs.body
-        #                                            FROM logs, log_recipients, users AS sender, users AS recipient
-        #                                            WHERE logs.id = log_recipients.log_id
-        #                                            AND sender.id = logs.author_id
-        #                                            AND recipient.id = log_recipients.recipient_id
-        #                                            AND logs.sent_on > %(startdate)s
-        #                                            LIMIT %(pagesize)s;
-        #                                            OFFSET %(offset)s """,
-        #                                            {'startdate': 0, 'pagesize': PAGESIZE, 'offset': offset})
-        return messages + whispers# + topics
-        
+    def db_fetch_messages(self, offset):
+        return self.db_execute_and_fetchall("""SELECT sender.name, recipient.name, messages.body
+                                               FROM messages, message_recipients, users as sender, users as recipient
+                                               WHERE messages.id = message_recipients.message_id
+                                               AND messages.parent_command_id IS NULL
+                                               AND messages.sender_id IS NOT NULL
+                                               AND sender.id = messages.sender_id
+                                               AND recipient.id = message_recipients.recipient_id
+                                               AND messages.sent_on > %(startdate)s
+                                               LIMIT %(pagesize)s
+                                               OFFSET %(offset)s
+                                            """, {
+                                               'startdate': 0,
+                                               'pagesize': PAGESIZE,
+                                               'offset': offset
+                                            })
+    
+    def db_fetch_topics(self, offset):
+        return self.db_execute_and_fetchall("""SELECT sender.name, recipient.name, commands.string
+                                               FROM commands, messages, message_recipients, users as sender, users as recipient
+                                               WHERE commands.command_name = 'topic'
+                                               AND commands.sender_id = sender.id
+                                               AND commands.id = messages.parent_command_id
+                                               AND messages.id = message_recipients.message_id
+                                               AND recipient.id = message_recipients.recipient_id
+                                               AND sender.id != message_recipients.recipient_id
+                                               AND commands.string IS NOT NULL
+                                               AND commands.sent_on > %(startdate)s
+                                               LIMIT %(pagesize)s
+                                               OFFSET %(offset)s
+                                            """, {
+                                               'startdate': 0,
+                                               'pagesize': PAGESIZE,
+                                               'offset': offset
+                                            })
+    
+    def db_fetch_whispers(self, offset):
+        return self.db_execute_and_fetchall("""SELECT sender.name, recipient.name, messages.body
+                                               FROM commands, messages, message_recipients, users as sender, users as recipient
+                                               WHERE commands.command_name = 'whisper'
+                                               AND messages.id = message_recipients.message_id
+                                               AND messages.sender_id IS NOT NULL
+                                               AND sender.id = messages.sender_id
+                                               AND recipient.id = message_recipients.recipient_id
+                                               AND commands.id = messages.parent_command_id
+                                               AND commands.sent_on > %(startdate)s
+                                               LIMIT %(pagesize)s
+                                               OFFSET %(offset)s
+                                            """, {
+                                               'startdate': 0,
+                                               'pagesize': PAGESIZE,
+                                               'offset': offset
+                                            })
+    
+    def db_fetch_invites(self, offset):
+        return self.db_execute_and_fetchall("""SELECT sender.name, commands.token, messages.body
+                                               FROM commands, messages, message_recipients, users as sender
+                                               WHERE commands.command_name = 'invite'
+                                               AND commands.sender_id IS NOT NULL
+                                               AND sender.id = commands.sender_id
+                                               AND commands.is_valid IS TRUE
+                                               AND messages.sender_id IS NULL
+                                               AND messages.parent_message_id IS NULL
+                                               AND messages.parent_command_id = commands.id
+                                               AND messages.body NOT LIKE 'Sorry, %%'
+                                               #TODO fix this ugly hack, which assumes that if a response to a command begins with 'Sorry', 
+                                               # there was an ExecutionError, and all other responses indicate a successful command.
+                                               AND messages.id = message_recipients.message_id
+                                               AND message_recipients.recipient_id = sender.id
+                                               AND commands.sent_on > %(startdate)s
+                                               LIMIT %(pagesize)s
+                                               OFFSET %(offset)s
+                                            """, {
+                                               'startdate': 0,
+                                               'pagesize': PAGESIZE,
+                                               'offset': offset
+                                            })
+    
+    def db_fetch_kicks(self, offset):
+        return self.db_execute_and_fetchall("""SELECT sender.name, commands.token, messages.body
+                                               FROM commands, messages, message_recipients, users as sender
+                                               WHERE commands.sender_id IS NOT NULL
+                                               AND sender.id = commands.sender_id
+                                               AND commands.sent_on > %(startdate)s
+                                               AND commands.is_valid IS TRUE
+                                               AND commands.command_name = 'kick'
+                                               AND messages.sender_id IS NULL
+                                               AND messages.parent_message_id IS NULL
+                                               AND messages.parent_command_id = commands.id
+                                               AND messages.body NOT LIKE 'Sorry, %%'
+                                               AND messages.id = message_recipients.message_id
+                                               AND message_recipients.recipient_id = sender.id
+                                               LIMIT %(pagesize)s
+                                               OFFSET %(offset)s
+                                            """, {
+                                               'startdate': 0,
+                                               'pagesize': PAGESIZE,
+                                               'offset': offset
+                                            })    
     
     def db_execute_and_fetchall(self, query, data={}, strip_pairs=False):
         self.db_execute(query, data)
@@ -152,6 +199,7 @@ class EdgeCalculator(sleekxmpp.ClientXMPP):
         return []
     
     def db_execute(self, query, data={}):
+        logging.info(query % data)
         if not self.db or not self.cursor:
             logging.info("Database connection missing, attempting to reconnect and retry query")
             if self.db:
@@ -180,7 +228,7 @@ class EdgeCalculator(sleekxmpp.ClientXMPP):
     
     def cleanup(self):
         if self.db:
-            self.db.close()  
+            self.db.close()
         sys.exit(1)
     
 
