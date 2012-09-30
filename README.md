39a939dfcbccc3046c4d47727fa846a54df8d098
Steven Lehrburger <lehrburger@gmail.com>
2012-09-30 03:19:04 -0400

    Add instructions and an installation shell script
    Installing JWChat from the repository source requires multiple steps, so it seemed easiest to encapsulate it in a little script. First, this script configures the server, username, and password. Then, it optionally checks out the Debugger files, and enables the flag. Finall,y, it removes any previous files from the nginx directory, and transfers over the new ones.

--- README.md
+++ README.md
@@ -4,5 +4,13 @@ Development Setup
   * TODO
 0. Optionally set up the vine.im home page form
   * TODO
-0. Add JWChat files
-  * TODO
+0. Install necessary Perl modules
+  * `cpan`
+  * `o conf urllist`  # Make sure there are valid mirrors, and if not, try adding the following
+  * `o conf urllist push http://cpan.strawberryperl.com/`
+  * `o conf commit`
+  * Control-d to leave the cpan prompt
+  * `sudo cpan Locale::Maketext::Fuzzy`  # And repeat for any other necessary modules, noting that error messages like "Can't locate Locale/Maketext/Fuzzy.pm in @INC" mean you should try commands like the one mentioned
+0. Fetch, configure, and install JWChat
+  * `git clone git@github.com:lehrblogger/vine-jwchat.git jwchat`
+  * `sudo sh install.sh [vine.im or dev.vine.im] [username] [password] [random string for URL] [optional debug flag]`
---
+++ install.sh
@@ -0,0 +1,25 @@
+if [ $# -le 1 ]
+then
+  echo "$0 : You must supply the following 4 or 5 arguments: domain, username, password, URL hash, debug_flag"
+  exit 1
+fi
+
+#make
+cp src/config.js.example htdocs.en/config.js
+cd htdocs.en
+perl -pi -e 's/var SITENAME = "localhost";/var SITENAME = "'$1'";/g' config.js
+perl -pi -e 's/httpbase:"\/http-bind\/"/httpbase:"http:\/\/'$1':5280\/http-bind\/"/g' config.js
+perl -pi -e 's/var GUEST_ACC = "";/var GUEST_ACC = "'$2'";/g' config.js
+perl -pi -e 's/var GUEST_PWD = "";/var GUEST_PWD = "'$3'";/g' config.js
+if [ $5 ]
+then
+  git clone git://github.com/lehrblogger/JSDebugger.git
+  mv JSDebugger/* ./
+  rm -r JSDebugger
+  perl -pi -e 's/var DEBUG = false;/var DEBUG = true;/g' config.js
+fi
+
+cd ..
+rm -rf /usr/local/nginx/html/$4
+mkdir /usr/local/nginx/html/$4/
+cp -r htdocs.en /usr/local/nginx/html/$4/demo
