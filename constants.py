78e82e07b3777eb865330fd5629e385b0fd016fc
Steven Lehrburger <lehrburger@gmail.com>
2012-11-13 17:51:01 -0500

    Add MySQL and SQS info for celery

--- constants.py
+++ constants.py
@@ -12,4 +12,12 @@ web_mysql_password = env_vars.web_mysql_password
 db_host = env_vars.db_host
 db_name = 'vine'
 
+celery_mysql_user = 'celery'
+celery_mysql_password = env_vars.celery_mysql_password
+celery_db_name = 'celery_db'
+
+aws_sqs_prefix = 'vine-dev-'
+aws_access_key_id = env_vars.aws_access_key_id
+aws_secret_access_key = env_vars.aws_secret_access_key  # if it's broken, regenerate this until you get one without a "/"
+
 flask_secret_key = env_vars.flask_secret_key
