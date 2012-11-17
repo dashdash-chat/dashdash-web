4e2b65cefc83e795866990859f2b1ec928a87df1
jbergen <jbergen@gmail.com>
2012-11-17 16:50:53 -0500

    updated header and sidebar

--- flask_app.py
+++ flask_app.py
@@ -90,6 +90,27 @@ def demo(token):
         return render_template('demo.html', server=constants.domain, username=found_demo['name'], password=found_demo['password'])
     return redirect(url_for('index'))
 
+
+@app.route("/about")
+def about():
+    user = session.get('vine_user')
+    return render_template('about.html', user=user)
+
+@app.route("/legal")
+def legal():
+    user = session.get('vine_user')
+    return render_template('legal.html', user=user)
+
+@app.route("/setup")
+def setup():
+    user = session.get('vine_user')
+    return render_template('setup.html', user=user)
+
+@app.route("/contacts")
+def contacts():
+    user = session.get('vine_user')
+    return render_template('contacts.html', user=user)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
