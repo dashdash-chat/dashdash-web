ec766d17246c0b2551f1e2dc9ff66c6f08dc68f3
Steven Lehrburger <lehrburger@gmail.com>
2012-11-19 00:39:53 -0500

    Add more JID constants to env_vars.py

--- constants.py
+++ constants.py
@@ -3,12 +3,15 @@ from shared import env_vars
 domain = env_vars.domain
 server_ip = env_vars.server_ip
 
+leaves_jid_user = env_vars.leaves_jid_user
+leaves_jid = env_vars.leaves_jid
+
 graph_mysql_user = 'graph'
 graph_mysql_password = env_vars.graph_mysql_password
 db_host = env_vars.db_host
 db_name = 'vine'
 
-graph_xmpp_user = '_graph'
+graph_xmpp_jid = env_vars.graph_xmpp_jid
 graph_xmpp_password = env_vars.graph_xmpp_password
 
 client_port = env_vars.client_port
