32865498623f78ba4a19741b6cdaea05ea0f143d
Steven Lehrburger <lehrburger@gmail.com>
2012-11-20 22:38:17 -0500

    Integrate vine-graph constants into web constants

--- constants.py
+++ constants.py
@@ -2,14 +2,21 @@ from shared import env_vars
 debug = env_vars.debug
 
 domain = env_vars.domain
+server_ip = env_vars.server_ip
 xmlrpc_server = env_vars.xmlrpc_server
 
 xmlrpc_port = env_vars.xmlrpc_port
 web_xmlrpc_user  = '_web'
 web_xmlrpc_password = env_vars.web_xmlrpc_password
+leaves_jid_user = env_vars.leaves_jid_user
+leaves_jid = env_vars.leaves_jid
+graph_xmpp_jid = env_vars.graph_xmpp_jid
+graph_xmpp_password = env_vars.graph_xmpp_password
 
 web_mysql_user = 'web'
 web_mysql_password = env_vars.web_mysql_password
+graph_mysql_user = 'graph'
+graph_mysql_password = env_vars.graph_mysql_password
 db_host = env_vars.db_host
 db_name = 'vine'
 
@@ -25,3 +32,5 @@ aws_access_key_id = env_vars.aws_access_key_id
 aws_secret_access_key = env_vars.aws_secret_access_key  # if it's broken, regenerate this until you get one without a "/"
 
 flask_secret_key = env_vars.flask_secret_key
+
+client_port = env_vars.client_port
