f85002d82efd7d380ae052430c1827775d2a7b31
Steven Lehrburger <lehrburger@gmail.com>
2012-11-26 13:42:10 -0500

    Don't reconnect constantly if xmpp server is down
    Otherwise it would try over and over without a delay, and while I thought SleekXMPP had some sort of exponential backoff thing, this is probably the right way to handle failure anyway.

--- celery_tasks.py
+++ celery_tasks.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 from flask.ext.oauth import OAuth
 from kombu import Exchange, Queue
 from sqlalchemy import create_engine, select, and_, MetaData, Table
+from time import sleep
 import constants
 from graph import EdgeCalculator
 
@@ -97,8 +98,14 @@ def score_edges(user_id=None):
     calculator.register_plugin('xep_0004') # Data Forms
     calculator.register_plugin('xep_0060') # PubSub
     calculator.register_plugin('xep_0199') # XMPP Ping
-    if calculator.connect((constants.server_ip, constants.client_port)):# caused a weird _der_cert error
+    if calculator.connect((constants.server_ip, constants.client_port), reattempt=False):# caused a weird _der_cert error
         calculator.process(block=True)
         logger.info("Done")
     else:
-        logger.info("Unable to connect.")
+        logger.info("Unable to connect, will retry in 3 seconds.")
+        sleep(3)
+        if calculator.connect((constants.server_ip, constants.client_port), reattempt=False):
+            calculator.process(block=True)
+            logger.info("Done")
+        else:
+            logger.info("Unable to connect after retry.")
