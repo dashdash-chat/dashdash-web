ae7343feee9887be7174bc77561f78fdbf5cfe0e
Steven Lehrburger <lehrburger@gmail.com>
2012-12-23 00:09:54 -0500

    Make sure the vine_web cookbook renders nginx.conf
    This is pretty simple, and seems to work.
    
    Note that I had accidentally done all of the work so far in the alpha branch of vine-web, so I nuked those commits from github and re-did it here.

--- cookbooks/vine_web/recipes/default.rb
+++ cookbooks/vine_web/recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+template "nginx.conf" do
+  path "#{node['nginx']['dir']}/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[nginx]'
+end
