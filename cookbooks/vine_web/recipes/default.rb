d4aebb5e6e31781f1b1377872a08c867a93d03cb
Steven Lehrburger <lehrburger@gmail.com>
2012-12-23 00:06:11 -0500

    Update cookbooks/vine_web/metadata.rb with basic info

--- cookbooks/vine_web/recipes/default.rb
+++ cookbooks/vine_web/recipes/default.rb
@@ -2,7 +2,9 @@
 # Cookbook Name:: vine_web
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Vine.IM
 #
 # All rights reserved - Do Not Redistribute
 #
+
+depends "nginx", "~> 1.1.2"
