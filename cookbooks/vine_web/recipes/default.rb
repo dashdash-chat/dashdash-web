9d22448f7660c7abd48e0259ff475df0eae92a6e
Steven Lehrburger <lehrburger@gmail.com>
2012-12-23 01:38:44 -0500

    Render nginx.conf template with a data_bag value!i
    Oh wow was this difficult to get working, and I have no idea why.

--- cookbooks/vine_web/recipes/default.rb
+++ cookbooks/vine_web/recipes/default.rb
@@ -7,11 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
+#TODO this should be in a cookbook for role[base] in the vine-chef repo, I think, but don't know how to do that yet with Berkshelf and etc
+env_data = data_bag_item("dev_data", "dev_data")
+
 template "nginx.conf" do
   path "#{node['nginx']['dir']}/nginx.conf"
   source "nginx.conf.erb"
   owner "root"
   group "root"
   mode 0644
+  variables :env_data => env_data
   notifies :reload, 'service[nginx]'
 end
