dcbd149d90f8b4fcbced234d235e12d7ce07cdfa
Steven Lehrburger <lehrburger@gmail.com>
2012-12-24 01:08:26 -0500

    Re-add SSL support in nginx
    Note that we can keep our SSL certificates in our data bag, which means they can be encrypted along with everything else in prod :)

--- cookbooks/vine_web/recipes/default.rb
+++ cookbooks/vine_web/recipes/default.rb
@@ -8,7 +8,32 @@
 #
 
 #TODO this should be in a cookbook for role[base] in the vine-chef repo, I think, but don't know how to do that yet with Berkshelf and etc
+#node.chef_environment
 env_data = data_bag_item("dev_data", "dev_data")
+
+directory "#{node['source_dir']}" do
+  mode 0644
+  owner "root"
+  group "root"
+  action :create
+end
+
+template "ssl_web.crt" do
+  path "#{node['source_dir']}/ssl_web.crt"
+  source "ssl.crt.erb"
+  owner "root"
+  group "root"
+  variables :ssl_crt => env_data["server"]["web_ssl_crt"]
+  mode 0644
+end
+template "ssl_web.key" do
+  path "#{node['source_dir']}/ssl_web.key"
+  source "ssl.key.erb"
+  owner "root"
+  group "root"
+  variables :ssl_key => env_data["server"]["web_ssl_key"]
+  mode 0644
+end
 
 template "nginx.conf" do
   path "#{node['nginx']['dir']}/nginx.conf"
