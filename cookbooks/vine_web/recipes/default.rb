e8baba1268c39fa20286a3fba6389416c08e6d2e
Steven Lehrburger <lehrburger@gmail.com>
2012-12-27 01:37:38 -0500

    Start to add MySQL config for dev only
    I'm probably going to be too nervous in prod to let chef anywhere near my database :)

--- cookbooks/vine_web/recipes/default.rb
+++ cookbooks/vine_web/recipes/default.rb
@@ -97,6 +97,22 @@ template "ssl_web.key" do
   mode 0644
 end
 
+if node.chef_environment == "dev"
+  node.set['mysql']['server_root_password'] = env_data["mysql"]["root_password"]
+  mysql_connection_info = {:host => env_data["mysql"]["host"], :username => 'root', :password => env_data["mysql"]["root_password"]}
+  [[env_data["mysql"]["web_user"],    env_data["mysql"]["web_password"]],
+   [env_data["mysql"]["graph_user"],  env_data["mysql"]["graph_password"]],
+   [env_data["mysql"]["celery_user"], env_data["mysql"]["celery_password"]],
+   [env_data["mysql"]["leaves_user"], env_data["mysql"]["leaves_password"]]
+  ].each do |user_password|
+    mysql_database_user user_password[0] do
+      connection mysql_connection_info
+      password user_password[1]
+      action :create
+    end
+  end
+end
+
 template "nginx.conf" do
   path "#{node['nginx']['dir']}/nginx.conf"
   source "nginx.conf.erb"
