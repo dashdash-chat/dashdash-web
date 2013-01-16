83e236a5d92ed976c0a193f0ef6c4b249fd80895
Steven Lehrburger <lehrburger@gmail.com>
2013-01-16 02:50:43 -0500

    Launch JWChat on new user signup & password change

---
+++ static/js/demo.js
@@ -0,0 +1,22 @@
+var jid, pass, register, prio, connect_host, connect_port, connect_secure;
+var jwchats = new Array();
+var JABBERSERVER;
+var HTTPBASE;
+var BACKEND_TYPE;
+function launchDemoClient(username, password) {
+    jid = username + "@" + JABBERSERVER + "/" + DEFAULTRESOURCE + Math.round(Math.random()*1000);
+    pass = password;
+    register = false;
+    prio = DEFAULTPRIORITY;
+    connect_secure = false;
+    jwchats[jid] = window.open("/static/jwchat/jwchat.html",makeWindowName(jid),'width=320,height=390,resizable=yes');
+    return true;
+}
+function init() {
+    var servers_allowed = BACKENDS[0].servers_allowed,
+        default_server = BACKENDS[0].default_server;
+    HTTPBASE = BACKENDS[0].httpbase;
+    BACKEND_TYPE = BACKENDS[0].type;
+    JABBERSERVER = servers_allowed[0];
+}
+onload = init;
