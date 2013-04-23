fe0cc8cd590718eaf501ace93dfdea86f025d911
Steven Lehrburger <lehrburger@gmail.com>
2013-04-23 14:09:54 -0400

    Clean up Javascript whitespace

--- static/js/home.js
+++ static/js/home.js
@@ -1,58 +1,57 @@
 $(window).ready(function(){
-	
-	var kids = $('.dashdash-intro-convo').children('.dashdash-chat-bubble');
-	//setTimeout(function(){ chatLeft(kids[0]); },1000);
-	setTimeout(function(){ puff(kids[1]); },1000);
-	setTimeout(function(){ puff(kids[2]); },2000);
-	setTimeout(function(){ puff(kids[3]); },2500);
-	setTimeout(function(){ puff(kids[4]); },3000);
-	setTimeout(function(){ puff(kids[5]); },3250);
-	setTimeout(function(){ puff(kids[6]); },3500);
-
-	// if a flash container is present then make it closable
-	if( $('.flash-container').length )
-	{
-		$('.flash-container .close').click(function(){
-			$('.flash-container').hide('blind',{'direction':'vertical'},'fast');
-			return false;
-		});
-	}
-
-	// if a list request link exists, then activate a form
-	if( $('.list-request').length )
-	{
-		$('.list-request').click(function(){
-
-			$('.list-request-container .hidden-form').toggle('blind',{'direction':'vertical'},'fast');
-			return false;
-		});
-		$('.list-request-container .cancel-request').click(function(){
-			$('.list-request-container input').val('');
-			$('.list-request-container .hidden-form').hide('blind',{'direction':'vertical'},'fast');
-			return false;
-		});
-
-		$('.list-request-container input').keyup(function(e){
-			if(e.which == 13)
-			{
-				console.log('email entered', $('.list-request-container input').val() );
-				var value = $('.list-request-container input').val();
-				if( isEmail(value) )
-				{
-					submitToList(value);
-					$('.list-request-container .hidden-form').hide('blind',{'direction':'vertical'},'slow');
-					$('.header-banner').effect('highlight',{},750);
-					$('.list-request').remove();
-					$('.list-request-container').prepend('<p>Thanks! Keep an eye on your inbox</p>');
-				}
-			}
-		});
-	}
+    
+    var kids = $('.dashdash-intro-convo').children('.dashdash-chat-bubble');
+    //setTimeout(function(){ chatLeft(kids[0]); },1000);
+    setTimeout(function(){ puff(kids[1]); },1000);
+    setTimeout(function(){ puff(kids[2]); },2000);
+    setTimeout(function(){ puff(kids[3]); },2500);
+    setTimeout(function(){ puff(kids[4]); },3000);
+    setTimeout(function(){ puff(kids[5]); },3250);
+    setTimeout(function(){ puff(kids[6]); },3500);
+    
+    // if a flash container is present then make it closable
+    if( $('.flash-container').length )
+    {
+        $('.flash-container .close').click(function(){
+            $('.flash-container').hide('blind',{'direction':'vertical'},'fast');
+            return false;
+        });
+    }
+    
+    // if a list request link exists, then activate a form
+    if( $('.list-request').length )
+    {
+        $('.list-request').click(function(){
+            $('.list-request-container .hidden-form').toggle('blind',{'direction':'vertical'},'fast');
+            return false;
+        });
+        $('.list-request-container .cancel-request').click(function(){
+            $('.list-request-container input').val('');
+            $('.list-request-container .hidden-form').hide('blind',{'direction':'vertical'},'fast');
+            return false;
+        });
+        
+        $('.list-request-container input').keyup(function(e){
+            if(e.which == 13)
+            {
+                console.log('email entered', $('.list-request-container input').val() );
+                var value = $('.list-request-container input').val();
+                if( isEmail(value) )
+                {
+                    submitToList(value);
+                    $('.list-request-container .hidden-form').hide('blind',{'direction':'vertical'},'slow');
+                    $('.header-banner').effect('highlight',{},750);
+                    $('.list-request').remove();
+                    $('.list-request-container').prepend('<p>Thanks! Keep an eye on your inbox</p>');
+                }
+            }
+        });
+    }
 
 });
 
 function puff(el) {
-	$(el).show('puff',{},500);	
+    $(el).show('puff',{},500);
 }
 
 function isEmail(email)
